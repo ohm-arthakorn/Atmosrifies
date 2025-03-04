c02b781e9d457ccc039859978f277692feebf503
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2025-03-04 19:37:40 +0700

    create with blynk and send a pm10 data

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,46 @@
-#include <Arduino.h>
+#define BLYNK_TEMPLATE_ID "TMPL6CJvPqOYw"
+#define BLYNK_TEMPLATE_NAME "Air Filtering"
+#define BLYNK_AUTH_TOKEN "ov9pT-x8NgNHIvmLpBkkP4qh0kUNCjEN"
+
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
 #include <SoftwareSerial.h>
+#include "Adafruit_SPIDevice.h"
+
 SoftwareSerial mySerial(13, 12); // RX, TX
-unsigned int pm1 = 0;
-unsigned int pm2_5 = 0;
-unsigned int pm10 = 0;
+int pm1 = 0;
+int pm2_5 = 0;
+int pm10 = 0;
+
+// WiFi Credentials
+const char *ssid = "Ohm";
+const char *password = "123456789";
 
 void setup()
 {
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(115200);
   mySerial.begin(9600);
+
+  // connect WiFi
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Connect to Blynk
+  Blynk.begin("ov9pT-x8NgNHIvmLpBkkP4qh0kUNCjEN", "Ohm", "123456789");
 }
 
-void loop()
+// Function to Read and Send PM Data to Blynk
+void readPMData()
 {
   int index = 0;
   char value;
@@ -21,7 +49,7 @@ void loop()
   while (mySerial.available())
   {
     value = mySerial.read();
-    if ((index == 0 && value != 0x42) || (index == 1 && value != 0x4d))
+    if ((index == 0 && value != 0x42) || (index == 1 && value != 0x4D))
     {
       Serial.println("Cannot find the data header.");
       break;
@@ -34,26 +62,14 @@ void loop()
     else if (index == 5)
     {
       pm1 = 256 * previousValue + value;
-      Serial.print("{ ");
-      Serial.print("\"pm1\": ");
-      Serial.print(pm1);
-      Serial.print(" ug/m3");
-      Serial.print(", ");
     }
     else if (index == 7)
     {
       pm2_5 = 256 * previousValue + value;
-      Serial.print("\"pm2_5\": ");
-      Serial.print(pm2_5);
-      Serial.print(" ug/m3");
-      Serial.print(", ");
     }
     else if (index == 9)
     {
       pm10 = 256 * previousValue + value;
-      Serial.print("\"pm10\": ");
-      Serial.print(pm10);
-      Serial.print(" ug/m3");
     }
     else if (index > 15)
     {
@@ -61,8 +77,24 @@ void loop()
     }
     index++;
   }
+
+  // Clear Serial Buffer
   while (mySerial.available())
     mySerial.read();
-  Serial.println(" }");
+
+  // Print Values
+  Serial.printf("{ \"pm1\": %d ug/m3, \"pm2_5\": %d ug/m3, \"pm10\": %d ug/m3 }\n", pm1, pm2_5, pm10);
+
+  // Send to Blynk Dashboard (Assign Virtual Pins)
+  Blynk.virtualWrite(V1, pm1);
+  Blynk.virtualWrite(V2, pm2_5);
+  Blynk.virtualWrite(V3, pm10);
+
   delay(1000);
+}
+
+void loop()
+{
+  Blynk.run();
+  readPMData();
 }
