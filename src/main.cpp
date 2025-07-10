a6a5613d10761425d490ae5680ce4099ca176c3d
ohm_arthakorn <ohm2549arthakorn@gmail.com>
2025-07-10 17:33:00 +0700

    ๊Update code for K Engineering

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
-#define RelayPin 27
 // ประกาศ Subscribe และ Publish ที่จะใช้ในการส่งข้อมูลเข้า Node Red
-#define Subscribe "sub"
-#define Publish "pub"
+#define RelayPin 21
+#define LED_Red 26
+#define LED_Green 25
+#define Subscribe "atmos/receive"
+#define Publish "atmos/send"
 
 #include <WiFi.h>
 #include <SoftwareSerial.h>
@@ -10,13 +12,10 @@
 #include <PubSubClient.h>
 
 SoftwareSerial mySerial(33, 32); // RX, TX
-int pm1 = 0;
-int pm2_5 = 0;
-int pm10 = 0;
 
 // * ส่วนจัดการกับการเชื่อมต่อต่าง ๆ ไม่ว่าจะเป็น WiFi หรือ MQTT
 
-// WiFi
+// * ส่วนของการจัดการเรื่องของการเชื่อมต่อ WiFi
 const char *ssid = "Ohm";
 const char *password = "123456789";
 
@@ -25,10 +24,11 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const int mqttPort = 1883;
-const char *mqttServer = "broker.mqtt.cool";
-const char *mqttClient = "test_1";
+const char *mqttServer = "test.mosquitto.org";
+const char *mqttClient = "";
 const char *mqttUsername = "";
 const char *mqttPassword = "";
+unsigned long initialMillis = millis();
 
 // จัดการส่งข้อมูลเข้า Node Red
 String DataString;
@@ -36,15 +36,25 @@ long lastMsg = 0;
 int value = 0;
 char msg[100];
 
+
+// * ส่วนสำหรับการสร้างตัวแปรที่เก็บค่าฝุ่นต่าง ๆ 
+int pm1 = 0;
+int pm2_5 = 0;
+int pm10 = 0;
+
 // ฟังก์ชันสำหรับการเชื่อมต่อ MQTT เข้ากับ Node Red
-void reconnect(){
-  while (!client.connected()){
+void reconnect()
+{
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
-    if(client.connect(mqttClient, mqttUsername, mqttPassword)){
+    if (client.connect(mqttClient, mqttUsername, mqttPassword))
+    {
       Serial.println("Connected !");
       client.subscribe(Subscribe);
     }
-    else{
+    else
+    {
       Serial.print("Failed, rc=");
       Serial.print(client.state());
       Serial.println("Try again in 5 seconds !");
@@ -54,21 +64,22 @@ void reconnect(){
 }
 
 // ฟังก์ชัน Callback สำหรับการรับข้อมูลจาก Node Red
-void callback(char* topic, byte* payload, unsigned int length){
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   String message;
 
-  for(int i = 0; i < length; i++){
+  for (int i = 0; i < length; i++)
+  {
     message = message + char(payload[i]);
   }
 
   Serial.println(message);
 }
 
-
-// I2C Scanner
+// * ฟังก์ชันสำหรับการเช็คการต่อ I2C
 void checkingi2c()
 {
   while (!Serial)
@@ -98,17 +109,15 @@ void checkingi2c()
   Serial.print("Found ");
   Serial.print(count, DEC);
   Serial.println(" device(s).");
- 
 }
 
-// * ฟังก์ชันสำหรับการอ่านค่า PM 2.5 และ PM 10 จากเซนเซอร์ อีกทั้งยังส่งข้อมูลไปยัง Node Red 
-void ReadandSendPM()
+// * ฟังก์ชันสำหรับการอ่านค่า PM และแสดงผลผ่าน Serial Monitor
+void ReadPM()
 {
+
   int index = 0;
   char value;
   char previousValue;
-  unsigned long initialMillis = millis();
-
   while (mySerial.available())
   {
     value = mySerial.read();
@@ -147,35 +156,29 @@ void ReadandSendPM()
 
   // Print Values
   Serial.printf("{ \"pm1\": %d ug/m3, \"pm2_5\": %d ug/m3, \"pm10\": %d ug/m3 }\n", pm1, pm2_5, pm10);
-
-
   delay(1000);
-  
-  // * ถ้า pm2.5 มีค่าเกิน 38 ug/m^3 จะทำการพ่นไอน้ำและส่งแจ้งเตือนผ่าน Node Red
-  if(pm2_5 > 38){
-    digitalWrite(RelayPin, 1);
-  }
-  else{
-    digitalWrite(RelayPin, 0);
-  }
+}
 
-  // * ส่งข้อมูลไปยัง Node Red
-  if(initialMillis - lastMsg > 1000){
+// * ฟังก์ชันสำหรับการอ่านค่า PM และส่งค่าขึ้น Node-Red
+void SendPM()
+{
+  if (initialMillis - lastMsg > 1000)
+  {
     lastMsg = initialMillis;
     ++value;
     DataString = "{ \"pm1\": " + String(pm1) + ", \"pm2_5\": " + String(pm2_5) + ", \"pm10\": " + String(pm10) + " }";
     DataString.toCharArray(msg, 100);
     Serial.print("Publish message: ");
     Serial.println(msg);
-    client.publish(Subscribe,msg);
+    client.publish(Subscribe, msg);
   }
   delay(1);
   client.loop();
 }
 
-
 // * ฟังก์ชันสำหรับการเชื่อมต่อ WiFi
-void connectedWiFi(){
+void connectedWiFi()
+{
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED)
@@ -192,13 +195,13 @@ void connectedWiFi(){
 void setup()
 {
   pinMode(RelayPin, OUTPUT);
-  digitalWrite(RelayPin, HIGH); // make sure Relay started with closed state ! 
+  digitalWrite(RelayPin, HIGH); // make sure Relay started with closed state !
   Wire.begin();
 
   Serial.begin(115200);
   mySerial.begin(9600);
 
-  // * ส่วนที่เกี่ยวกับการเชื่อมต่อ MQTT 
+  // * ส่วนที่เกี่ยวกับการเชื่อมต่อ MQTT
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
   client.subscribe(Subscribe);
@@ -211,8 +214,11 @@ void setup()
 // function that can control a foggy from blynk app
 void loop()
 {
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     reconnect();
   }
-  ReadandSendPM();
+
+  ReadPM();
+  SendPM();
 }
